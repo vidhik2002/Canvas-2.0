76b3fb6f885c52644ba81d3db8624a4084aef8f3
vidhik2002 <vidhik2002@gmail.com>
2024-03-12 11:05:26 +0530

    feat: added icons in Menu

---
+++ canvas-2.0/src/components/Menu/index.js
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPencil, faEraser, faRotateLeft, faRotateRight, faFileArrowDown, faR } from '@fortawesome/free-solid-svg-icons'
+
+const Menu = () => {
+    return(
+        <div> 
+            <div> 
+                <FontAwesomeIcon icon={faPencil} />
+            </div>
+            <div> 
+                <FontAwesomeIcon icon={faEraser} />
+            </div>
+            <div> 
+                <FontAwesomeIcon icon={faRotateLeft} />
+            </div>
+            <div> 
+                <FontAwesomeIcon icon={faRotateRight} />
+            </div>
+            <div> 
+                <FontAwesomeIcon icon={faFileArrowDown} />
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
