c15bda446790d3134fdf262f881500c8eecb2e2f
vidhik2002 <vidhik2002@gmail.com>
2024-03-15 11:43:37 +0530

    feat: integrating canvas api

--- canvas-2.0/src/components/Board/index.js
+++ canvas-2.0/src/components/Board/index.js
@@ -5,6 +5,24 @@ const Board = () => {
     const canvasRef = useRef(null);
     const activeMenuItem = useSelector((state) => state.menu.activeMenuItem)
     const {color, size} = useSelector((state) => state.toolbox[activeMenuItem])
+    const shouldDraw = useRef(false)
+
+    // updating configs
+    useEffect(() => {
+        if (!canvasRef.current) return
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d')
+
+        const changeConfig = (color, size) => {
+            context.strokeStyle = color
+            context.lineWidth = size
+        }
+        changeConfig(color, size)
+
+        
+    }, [color, size])
+
+    // mounting
     useEffect(() => {
         if (!canvasRef.current) return
         const canvas = canvasRef.current
@@ -13,9 +31,35 @@ const Board = () => {
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
 
+        const handleMouseDown = (e) => {
+            shouldDraw.current = true
+            context.beginPath()
+            context.moveTo(e.clientX, e.clientY)
+        }
+
+        const handleMouseMove = (e) => {
+            if (!shouldDraw.current) return
+            context.lineTo(e.clientX, e.clientY)
+            context.stroke()
+        }
+
+        const handleMouseUp = (e) => {
+            shouldDraw.current = false
+        }
+
+
+        canvas.addEventListener('mousedown', handleMouseDown)
+        canvas.addEventListener('mousemove', handleMouseMove)
+        canvas.addEventListener('mouseup', handleMouseUp)
+
+        return () => {
+            canvas.removeEventListener('mousedown', handleMouseDown)
+            canvas.removeEventListener('mousemove', handleMouseMove)
+            canvas.removeEventListener('mouseup', handleMouseUp)
+        }
+
     }, [])
 
-    console.log(color, size)
 
     return(
         <canvas ref={canvasRef}></canvas>
--- canvas-2.0/src/components/ToolBox/index.js
+++ canvas-2.0/src/components/ToolBox/index.js
@@ -35,7 +35,7 @@ const Toolbox = () => {
             {showBrushTool && <div className={styles.toolItem}>
                 <h4 className={styles.toolText}>Brush Size</h4>
                 <div className={styles.itemContainer}>
-                <input type="range" min={1} max={10} step={1} onChange={updateBrushSize}/>
+                <input type="range" min={1} max={10} step={1} onChange={updateBrushSize} value={size} />
                 </div>
             </div>}
             
